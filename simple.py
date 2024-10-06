11c4c3d2c0edfd6e70eeaa0136e273b6f8decc25
Tyler Arciniaga <tylerarc@umich.edu>
2024-10-06 14:30:44 -0400

    basic routing complete with form submission from user

--- simple.py
+++ simple.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 # this imports flask from Flask library and then also imports the session
 # MIGHT NEED TO LOOK INTO IMPORTING REDIRECT AND REQUEST!!!!
 
@@ -17,28 +17,37 @@ credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secre
 credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=credentials_manager)
 
-@app.route('/', methods = ["POST","GET"])
-def give_track_recs():
-    if request.method == "POST":
-        artist_name = request.form.get("artist_name")
-    
-    print(artist_name)
-    results = sp.search(q=artist_name, type="artist")
-    artist_id = results['artists']['items'][0]['id']  # Get the first artist ID
-    seed_artists = [artist_id]
-    recommendations = sp.recommendations(seed_artists=seed_artists)
-
+@app.route('/')
+def home():
     return render_template('index.html')
 
 
+@app.route('/process', methods = ["POST"])
+def process():
+    artist_name = ''
+    artist_name = request.form.get("artist_name")
+    '''
+    if request.method == "POST":
+        artist_name = request.form.get("artist_name")
+    '''
+    return redirect(url_for('result', artist_name = artist_name))
+
+@app.route('/result')
+def result():
+    artist_name = request.args.get('artist_name')
     
+    return artist_name
+
+
+
+
 '''
-    
-    
+    results = sp.search(q=artist_name, type="artist")
+    artist_id = results['artists']['items'][0]['id']  # Get the first artist ID
     
 
-      
-    
+    seed_artists = [artist_id]  
+    recommendations = sp.recommendations(seed_artists=seed_artists)
     
 '''
 
